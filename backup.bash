b465658bf1046d1e5900e894764dd6991b7903fc
CobaltCactus <ao3.cobaltcactus@gmail.com>
2025-01-01 20:11:50 -0600

    Backup at Wed Jan  1 20:11:49 CST 2025

--- backup.bash
+++ backup.bash
@@ -8,7 +8,7 @@ set -e;
 # MOD_SUBDIR_NAME="CircleOfTheSpores_db7e15fd-b2fc-b159-4bbd-1baab34d8c3a"
 # Look in "C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Projects"
 # for names of mods you have already started
-MOD_SUBDIR_NAME=""
+MOD_SUBDIR_NAME="volo_amorous_tattoos_vol_2_931e8051-1e74-9ac9-7886-6db828bbb39e"
 
 # This is the MinGW64 path to a Steam install of the toolkit
 BG3_DATA="/d/Program Files (x86)/Steam/steamapps/common/Baldurs Gate 3/Data"
